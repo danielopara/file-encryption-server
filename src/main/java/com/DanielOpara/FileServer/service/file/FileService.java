2c7d086ecd2211df1a80a35594a3bc3f86b59040
Daniel Opara <oparadaniv@gmail.com>
2024-10-29 13:23:08 +0100

    created FileService classes with the upload functionality

---
+++ src/main/java/com/DanielOpara/FileServer/service/file/FileService.java
@@ -0,0 +1,10 @@
+package com.DanielOpara.FileServer.service.file;
+
+import com.DanielOpara.FileServer.response.BaseResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface FileService {
+    BaseResponse fileUpload(String email, MultipartFile file) throws Exception;
+}
